5d47d59cb24af3d0d34b41bb4f08ab6c82b0af24
N1AKAI <rakansubs@gmail.com>
2024-11-26 15:44:41 +0100

    Add api routes to their specific file

--- wordpress-theme/routes/api.php
+++ wordpress-theme/routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Http\Controllers\LessonController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\LessonController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,7 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::apiResource('lessons', LessonController::class)->only(['store', 'update', 'destroy']);
+Route::get('lessons/course/{courseId}', [LessonController::class, 'findByCourse']);
 
