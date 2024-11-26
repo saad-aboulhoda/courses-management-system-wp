975628145d4b5c82db30d02142c59a095c1f4848
N1AKAI <rakansubs@gmail.com>
2024-11-26 15:53:54 +0100

    Add lesson controller for view

---
+++ wordpress-theme/app/Http/Controllers/LessonController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Lesson;
+use Illuminate\Http\Request;
+
+class LessonController extends Controller
+{
+    public function show($slug, Lesson $lesson) {
+        return view('lesson.show', compact('lesson'));
+    }
+}
